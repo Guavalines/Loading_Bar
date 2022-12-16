dd6db0aff408940f4870de9c6ec9f4e068c57bcc
guavalines <konstrictor@gmail.com>
2022-12-15 19:43:17 -0800

    added updateNum function

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const counterE1 = document.querySelector(".counter")
 const barE1 = document.querySelector(".loading-bar-front")
+
+let idx = 0;
+
+updateNum()
+
+function updateNum(){
+  counterE1.innerText = idx + "%"
+  barE1.style.width = idx + "%"
+}
